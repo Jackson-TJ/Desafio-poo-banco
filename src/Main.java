bd22aa6337c682a699608827c397841446840553
Jack_son <jacksoncoteminas@gmail.com>
2024-06-03 10:44:46 -0300

    Colocado teste

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cliente venilton = new Cliente();
-        venilton.setNome("Jackson");
+        Cliente jackson = new Cliente();
+        jackson.setNome("Jackson");
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        Conta cc = new ContaCorrente(jackson);
+        Conta poupanca = new ContaPoupanca(jackson);
 
         cc.depositar(150);
         cc.transferir(25, poupanca);
